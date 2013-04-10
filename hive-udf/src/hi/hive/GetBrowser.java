30fd26f978812b08ae951f1b89ed6fa132b5f86b
Mark Kerzner <markkerzner@gmail.com>
2013-04-10 16:54:46 -0500

    Hive UDF

---
+++ hive-udf/src/hi/hive/GetBrowser.java
@@ -0,0 +1,19 @@
+package com.shmsoft.webloganalyzer;
+
+/**
+ *
+ * @author mark
+ */
+import nl.bitwalker.useragentutils.UserAgent;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public final class GetBrowser extends UDF {
+
+    public Text evaluate(final Text s) {
+        if (s == null) {
+            return null;
+        }
+        return new Text(UserAgent.parseUserAgentString(s.toString()).getBrowser().getName());
+    }
+}
