07f98cf9bf24251cf034e9a7144b2bf80f12b390
Ubuntu <ubuntu@ip-10-100-150-245.ec2.internal>
2013-06-28 12:00:13 +0000

    fixing hive

---
+++ hive/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+build_dir=build
+mkdir -p $build_dir
+rm -rf $build_dir/*
+
+
+javac -d $build_dir  -sourcepath src -classpath lib/*:../lib/*:../lib/mrunit/*:../lib/hbase/*:../lib/hive/*  $(find src -name "*.java")
+
+
+rm  -f a.jar
+jar cf a.jar -C $build_dir .
+
+rm -rf $build_dir
---
+++ hive/compile2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# tmp hack for hadoop2 distros
+
+build_dir=build
+mkdir -p $build_dir
+rm -rf $build_dir/*
+
+
+javac -d $build_dir  -sourcepath src -classpath /usr/lib/hadoop/*:/usr/lib/hadoop-hdfs/*:/usr/lib/hadoop-0.20-mapreduce/*  $(find src -name "*.java")
+
+
+rm  -f a.jar
+jar cf a.jar -C $build_dir .
+
+rm -rf $build_dir
