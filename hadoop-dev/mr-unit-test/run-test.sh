78f3abfc0b91c87ff2706493fa85b5a6af5fdbc1
Sujee Maniyam (mac2) <sujee@sujee.net>
2013-07-22 00:28:53 -0700

    updating mrunit libraries & compile scripts

--- hadoop-dev/mr-unit-test/run-test.sh
+++ hadoop-dev/mr-unit-test/run-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp a.jar:../lib/*:../lib/mrunit/*  org.junit.runner.JUnitCore  hi.mr.MapperTest
+java -cp a.jar:../lib/*:../lib/mrunit-hadoop1/*  org.junit.runner.JUnitCore  hi.mr.MapperTest
