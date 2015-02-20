127140ab2909c7b490e4110e15349386be501cdf
Tim Fox <tim@elephantscale.com>
2015-02-20 11:56:32 -0500

    Added link to lib directory for compile.sh

--- hadoop-dev/compile.sh
+++ hadoop-dev/compile.sh
@@ -3,15 +3,16 @@
 ## compile script
 
 export HADOOP_VERSION=`hadoop version | grep Hadoop | sed 's/Hadoop [1-2]\.[0-9]\.[0-9]\.//g'`
+echo $HADOOP_VERSION
 
 build_dir=build
 mkdir -p $build_dir
 rm -rf $build_dir/*
 
 #classpath="$(hadoop classpath):$(hbase classpath)"
-export additional_classpath=/usr/hdp/$HADOOP_VERSION/hive/lib/*:/usr/hdp/$HADOOP_VERSION/pig/*:/usr/hdp/$HADOOP_VERSION/pig/lib/*
+export additional_classpath=/usr/hdp/$HADOOP_VERSION/hive/lib/*:/usr/hdp/$HADOOP_VERSION/pig/*:/usr/hdp/$HADOOP_VERSION/pig/lib/*:./lib/*
 classpath="$(hadoop classpath)":$additional_classpath
-#echo $classpath
+echo $classpath
 
 javac -d $build_dir  -sourcepath src -classpath "$classpath"  $(find src -name "*.java")
 
